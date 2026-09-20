720cde0a060ca561f298bc48725322124f8a4219
Isaac <isaac.chacon@upb.edu.co>
2026-09-20 14:35:41 -0500

    Completado main2.js con prompt y saludo personalizado (Commit 3)

---
+++ js/main2.js
@@ -0,0 +1,9 @@
+// Pedimos el nombre al usuario con una ventana prompt
+let nombre = prompt("isaac:");
+
+// Creamos la variable saludo con el texto literal + el nombre
+let saludo = "Cordial saludo " + isaac;
+
+// Mostramos el valor de saludo en un alert
+alert(saludo);
+
